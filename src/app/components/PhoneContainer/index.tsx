d5193ab2c24556512bce3a142720acd3c596c035
Ivan Kudriashov <kot91ivan@gmail.com>
2025-01-31 15:42:47 +0200

    Add fixes for hover photos

--- src/app/components/PhoneContainer/index.tsx
+++ src/app/components/PhoneContainer/index.tsx
@@ -20,7 +20,7 @@ const PhoneContainer: React.FC<PhoneContainerProps> = ({ isLarge, url, alt }) =>
         max: 15, // Максимальный угол наклона
         speed: 300, // Скорость анимации
         glare: true, // Добавить блик
-        "max-glare": 0.5 // Интенсивность блика
+        "max-glare": 0.2 // Интенсивность блика
       })
     }
 
@@ -29,9 +29,11 @@ const PhoneContainer: React.FC<PhoneContainerProps> = ({ isLarge, url, alt }) =>
     }
   }, [])
   return (
-    <div ref={tiltRef} className="relative rounded-2xl border-[1px] overflow-hidden scale-75 hover:scale-120">
-      <div className="h-[2px] absolute bottom-1 w-8 left-[50%] translate-x-[-50%] opacity-[0.8] rounded-sm bg-white"></div>
-      <Image width={100} height={120} src={url} className="w-full h-full" alt="123" />
+    <div className={` ${isLarge ? "scale-100" : "scale-90"} hover:scale-120`}>
+      <div ref={tiltRef} className="relative rounded-2xl border-[1px] overflow-hidden">
+        <div className="h-[2px] absolute bottom-1 w-8 left-[50%] translate-x-[-50%] opacity-[0.8] rounded-sm bg-white"></div>
+        <Image width={100} height={120} src={url} className="w-full h-full" alt="123" />
+      </div>
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,9 @@ export default function Home() {
           <MainContent />
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <h2></h2>
+      </footer>
       <Analytics />
     </div>
   )
