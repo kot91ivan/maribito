741c759cb4ed1c2d84a201e795ab3ca666f3c327
Ivan Kudriashov <kot91ivan@gmail.com>
2025-01-31 23:02:22 +0200

    add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link"
 import MainContent from "./components/MainContent"
 import Projects from "./components/Projects"
 import Sidebar from "./components/Sidebar"
 import { Analytics } from "@vercel/analytics/react"
+import Image from "next/image"
+
+const socialLinks = [
+  {
+    name: "Linkedin",
+    url: "#"
+  },
+  {
+    name: "Instagram",
+    url: "#"
+  }
+]
 
 export default function Home() {
   return (
@@ -14,8 +27,28 @@ export default function Home() {
         <Projects />
         <div></div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <h2></h2>
+      <footer className="row-start-3 flex gap-10 flex-wrap items-center justify-center bg-container rounded-t-3xl py-6">
+        <h2 className="font-black text-9xl lg:text-[160px]">lets collaborate!</h2>
+        <div className="flex items-center justify-between w-full px-8">
+          <div className="flex flex-col gap-4 items-start">
+            <h3 className="text-3xl lg:text-5xl font-bold">
+              Ready to create something <br /> <span className="text-accent">awesome together?</span>
+            </h3>
+
+            <button className="bg-accent text-black font-semibold text-xl py-3 px-6 rounded-xl">Contact me</button>
+          </div>
+
+          <div className="flex flex-col gap-2">
+            {socialLinks.map(link => {
+              return (
+                <Link key={link.name} href={link.url} className="flex items-center gap-2">
+                  <Image width={32} height={32} src={`/icons/${link.name}.png`} alt={link.name} />
+                  {link.name}
+                </Link>
+              )
+            })}
+          </div>
+        </div>
       </footer>
       <Analytics />
     </div>
